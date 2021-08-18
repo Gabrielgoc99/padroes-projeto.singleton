a2b74e3850df667fdebe912057c0dd5490657156
Gabrielgoc99 <gabrielgoc99@gmail.com>
2021-08-18 20:37:42 -0300

    Correção do nome da classe de testes.

--- src/test/java/MarcaTest.java
+++ src/test/java/MarcaTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ParametrosTest {
+class MarcaTest {
 
     @Test
     public void deveRetornarNomeMarca() {
